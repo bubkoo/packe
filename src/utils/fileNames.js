97db452472ec007a27f37ec02afabaa273fe2e0e
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2017-09-19 11:38:07 +0800

    fix mock config path

--- src/utils/fileNames.js
+++ src/utils/fileNames.js
@@ -1,13 +1,12 @@
 export const baseName = 'porsche';
+export const rcConfigFileName = `.${baseName}rc`;
 
 export const dllModulePath = `node_modules/${baseName}-dll`;
 export const dllBundleFileName = `${baseName}.dll.js`;
 export const dllManifestFileName = `${baseName}.manifest.json`;
 
-export const mockConfigFileName = `.${baseName}.mode.js`;
 export const webpackConfigFileName = 'webpack.config.js';
-
-export const rcConfigFileName = `.${baseName}rc`;
+export const mockConfigFileName = `${rcConfigFileName}.mock.js`;
 export const jsConfigFileName = `${rcConfigFileName}.js`;
 export const ymlConfigFileName = `${rcConfigFileName}.yml`;
 export const yamlConfigFileName = `${rcConfigFileName}.yaml`;
